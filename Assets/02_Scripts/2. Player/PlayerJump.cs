0dc8729b7a238e56b19ad2b516a7755088e481a3
Namsojeong <nsj050320@gmail.com>
2022-06-30 17:55:29 +0900

    2단 점프 완료

--- Assets/02_Scripts/2. Player/PlayerJump.cs
+++ Assets/02_Scripts/2. Player/PlayerJump.cs
@@ -7,9 +7,9 @@ public class PlayerJump : Character
     [SerializeField]
     private float jumpPower = 10;
 
+    bool isJump;
+
     EventParam eventParam = new EventParam();
-    bool isFirst = false;
-    private bool isJump = false;
 
     private readonly int hashJumpOn = Animator.StringToHash("JumpStart");
     private readonly int hashJumpOff = Animator.StringToHash("JumpStop");
@@ -23,34 +23,27 @@ public class PlayerJump : Character
     protected override void Update()
     {
         ani.SetFloat("velocity", rigidbody.velocity.y);
-        if (rigidbody.velocity.y <= 0)
-        {
-            isJump = false;
-        }
-        Debug.Log($"rigid velocity y : {rigidbody.velocity.y}");
+
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        isJump = false;
         ani.SetBool(hashJumpOff, true);
+        isJump = false;
+        
     }
 
     void JumpOn(EventParam eventParam)
     {
-        if (rigidbody.velocity.y <= 0)
-            isJump = true;
+        if (isJump) return;
+        isJump = true;
         ani.SetTrigger(hashJumpOn);
         Jump();
     }
 
     void Jump()
     {
-        if (isJump)
-        {
-            rigidbody.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
-        }
-
+        rigidbody.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
     }
 
     private void OnDestroy()

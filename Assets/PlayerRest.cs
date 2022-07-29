ea563268b7fe3b228533ab652a7307b66c7f3449
Namsojeong <nsj050320@gmail.com>
2022-07-29 15:14:49 +0900

    차 마시는 애니메이션 추가

--- Assets/PlayerRest.cs
+++ Assets/PlayerRest.cs
@@ -6,10 +6,16 @@ public class PlayerRest : MonoBehaviour
 {
     [SerializeField]
     ParticleSystem spawnParticle;
+    [SerializeField]
+    GameObject cup;
 
     Animator anim;
+    private readonly int hashDrink = Animator.StringToHash("Drinking");
 
     Vector3 savePoint = Vector3.zero;
+    bool isRest = false;
+
+    EventParam eventParam = new EventParam();
 
     private void Awake()
     {
@@ -47,10 +53,29 @@ public class PlayerRest : MonoBehaviour
     }
     void EffectSave()
     {
+        if (isRest) return;
+        isRest = true;
         // 스폰 저장 이펙트
         // 플레이어 차 마시는 애니메이션 추가
-        spawnParticle.Play();
+        cup.SetActive(true);
+        MoveStop();
+        anim.SetTrigger(hashDrink);
+        Invoke("MoveStart", 5f);
+        //spawnParticle.Play();
     }
 
+    void MoveStop()
+    {
+        eventParam.boolParam = false;
+        EventManager.TriggerEvent("ISMOVE", eventParam);
+    }
+    
+    void MoveStart()
+    {
+        isRest = false;
+        cup.SetActive(false);
+        eventParam.boolParam = true;
+        EventManager.TriggerEvent("ISMOVE", eventParam);
+    }
 
 }
